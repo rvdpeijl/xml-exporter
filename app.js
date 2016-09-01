73b006f48ef6bec6f807ef7ed820708b448cce44
Robbin van der Peijl <robbin.vanderpeijl@digitaslbi.com>
2016-09-01 12:56:36 +0200

    added dev server

---
+++ app.js
@@ -0,0 +1,11 @@
+var path = require("path")
+
+var express = require("express")
+var app     = express()
+
+app.use(express.static(path.join(__dirname, "build")))
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000")
+})
+
